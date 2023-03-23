51aab54cdb47b5614acfd65b6581001703a3b390
Kavin <kavin.avs@gmail.com>
2023-03-23 00:52:12 -0400

    adds presistent storage of grades, starts on tabs for multiple grade tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,36 @@
 <script>
-	
+    import { space } from "svelte/internal";
+	import Calculator from "./Calculator.svelte";
+
+	let current_tab = "default";
+
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div class="sm:hidden">
+    <label for="tabs" class="sr-only">Select your country</label>
+    <select id="tabs" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option>Profile</option>
+        <option>Canada</option>
+        <option>France</option>
+        <option>Germany</option>
+    </select>
+</div>
+<ul class="hidden sm:flex text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow  dark:divide-gray-700 dark:text-gray-400">
+    <li class="w-full">
+        <a href="#" class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">Profile</a>
+    </li>
+    <li class="w-full">
+        <a href="#" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Dashboard</a>
+    </li>
+    <li class="w-full">
+        <a href="#" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Settings</a>
+    </li>
+    <li class="w-full">
+        <a href="#" class="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Invoice</a>
+    </li>
+</ul>
 
-<section>
-	
-</section>
+<div class="mt-8" ><Calculator table_name={current_tab}/></div>
 
 <style>
 
---
+++ src/routes/Calculator.svelte
@@ -0,0 +1,89 @@
+<script>
+	export let table_name; 
+
+	let aim = 80;
+	let count = 7;
+	let marks = Array(count);
+	let weights = Array(count);
+	let names = Array(count);
+	
+	if( table_name in localStorage ){
+		let vals = JSON.parse(localStorage.getItem("default"));
+		count = vals.marks.length;
+		marks = vals.marks;
+		weights = vals.weights;
+		names = vals.names;
+	}
+
+	function removeEntry(id){
+		marks = [...marks.slice(0, id), ...marks.slice(id + 1)];
+		weights = [...weights.slice(0, id), ...weights.slice(id + 1)];
+		names = [...names.slice(0, id), ...names.slice(id + 1)];
+		count = count - 1;
+	}
+
+	$:total_weight = weights.reduce((sum, n) => sum+n,0);
+	$:completed_average = marks.reduce((sum, n, i) =>{ return (sum + (n*weights[i])) }, 0)
+	$:current_average =  completed_average/total_weight;
+	$:needed = (aim - (completed_average/100))/ (100-total_weight)*100;
+
+	$:{saveToLocalStrg(names,marks,weights);}
+
+	function saveToLocalStrg(names, marks, weights){
+		localStorage.setItem(table_name, JSON.stringify({"names": names, "marks": marks, "weights": weights}));
+	}
+
+</script>
+
+<svelte:head>
+	<title>Grade Calculator</title>
+	<meta name="grade-calculator" content="" />
+</svelte:head>
+
+<div class="min-w-full flex justify-center items-center flex-col">
+	<form method="post" class="max-w-xl columns-1 w-11/12">
+		<div class="gap-2 md:gap-4 flex mb-2">
+			<h5 class="w-1/2">Name</h5>
+			<h5 class="w-1/4">Mark</h5>
+			<h5 class="w-1/4">Weight</h5>
+			<p class="w-12"></p>
+		</div>
+		{#each Array(count) as _, i (i)}
+			<div class="gap-2 md:gap-4 min-w-full flex justify-evenly mb-2">
+				<input name={`name${i}`} bind:value={names[i]} type="text" class="w-1/2 border-solid border-2 border-neutral-500 rounded-md p-2">
+				<input name={`mark${i}`} bind:value={marks[i]} min=0 max=100 type="number" placeholder="%" class="w-1/4 border-solid border-2 border-neutral-500 rounded-md p-2 text-right">
+				<input name={`weight${i}`} bind:value={weights[i]} min=0 max=100 type="number" class="w-1/4 border-solid border-2 border-neutral-500 rounded-md p-2 text-right">
+				<button class="btn" on:click={removeEntry(i)}>-</button>
+			</div>
+		{/each}
+		<button class="btn w-full" on:click={() => count = count + 1 }>+</button>
+	</form>
+	<p>Average: {current_average.toFixed(2)}%</p>
+	<span>What do I need to get: <input name="aim" bind:value={aim} min=0 max=100 type="number" class="border-solid border-2 border-neutral-500 rounded-md px-2 py-1 text-right">%</span>
+	<p>Whats needed: {needed.toFixed(2)}%</p>
+</div>
+
+
+<style>
+input::-webkit-outer-spin-button,input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
+.btn {
+	@apply bg-indigo-300 text-white font-bold rounded-md p-2 px-4 text-center;
+}
+.btn:hover{
+	@apply bg-indigo-400;
+}
+
+.btn:active{
+	@apply bg-indigo-500;
+}
+
+</style>
