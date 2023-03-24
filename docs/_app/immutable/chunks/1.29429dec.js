64fe322d5da6041bdd2133e432a9059bd378a7ba
Kavin <kavin.avs@gmail.com>
2023-03-24 14:07:55 -0400

    changes favicon

--- docs/_app/immutable/chunks/1.29429dec.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2b9677bc.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.eb56c77f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.d0b4becd.js";export{t as component};
