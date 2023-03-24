697ed226065d56ed0ed6d8f995dbef92a90b883f
Kavin <kavin.avs@gmail.com>
2023-03-24 14:00:51 -0400

    changes build config for extra ghpages files

--- docs/_app/immutable/chunks/1.05c54c90.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c3dd8d69.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.29429dec.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.2b9677bc.js";export{t as component};
